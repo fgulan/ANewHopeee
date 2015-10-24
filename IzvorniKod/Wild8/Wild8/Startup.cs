56b188782719067dd4566c8dfe1ce961329448cd
Filip Gulan <filip.gulan@fer.hr>
2015-10-24 12:20:31 +0200

    Created basic project structure in VS2015.

---
+++ IzvorniKod/Wild8/Wild8/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Wild8.Startup))]
+namespace Wild8
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
