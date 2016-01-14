09f007f503a223e74a6fe98dac6cd0e8898eec54
Domagoj Latečki <domagoj1994@gmail.com>
2016-01-14 12:32:26 +0100

    Mail for bill is now working.

--- IzvorniKod/Wild8/Wild8/Startup.cs
+++ IzvorniKod/Wild8/Wild8/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Owin;
 using Owin;
 using Microsoft.AspNet.SignalR;
 using Wild8.Hubs.Utils;
+using Wild8.Utils;
+using Wild8.StaticInfo;
 
 [assembly: OwinStartup(typeof(Wild8.Startup))]
 
@@ -15,6 +17,7 @@ namespace Wild8
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
             GlobalHost.HubPipeline.AddModule(new ErrorHandlingPipelineModule());
+            MailUtil.Initialize(StaticInfoMap.RestMailSmtpClient, StaticInfoMap.RestEmail, StaticInfoMap.RestEmail, StaticInfoMap.RestEmailPass);
             var config = new HubConfiguration();
             config.EnableDetailedErrors = true;
             app.MapSignalR(config);
